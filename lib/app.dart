2f8ce6bacfa5fc653f7c3d79a7bc7dae6da69741
Damodar Lohani <dlohani48@gmail.com>
2025-01-31 07:30:43 +0545

    hide banner

--- lib/app.dart
+++ lib/app.dart
@@ -7,6 +7,7 @@ class FontPreviewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Font preview",
       home: HomeDesktopScreen(),
     );
