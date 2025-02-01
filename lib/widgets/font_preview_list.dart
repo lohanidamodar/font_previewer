1657a8cdf7324c4310e57dad6f9b4caa64dfc1f2
Damodar Lohani <Dlohani48@gmail.com>
2025-02-01 11:59:29 +0545

    launch font details

--- lib/widgets/font_preview_list.dart
+++ lib/widgets/font_preview_list.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:font_previewer/models/font_family.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:path/path.dart' as p;
 
 class FontPreviewList extends StatelessWidget {
   final Iterable<FontFamily> fontFamilies;
@@ -25,9 +30,20 @@ class FontPreviewList extends StatelessWidget {
                 message: fontFamilies.elementAt(index).path,
                 child: IconButton(
                     icon: Icon(Icons.info),
-                    onPressed: () {
+                    onPressed: () async {
                       final details = fontFamilies.elementAt(index);
-                      if (details.isLocal) {}
+                      if (details.isLocal) {
+                        final path = details.path;
+
+                        final dir = p.dirname(path);
+                        launchUrl(Uri.parse(dir));
+                      } else {
+                        if (await canLaunchUrlString(
+                            'https://fonts.google.com/specimen/${details.path}')) {
+                          launchUrlString(
+                              'https://fonts.google.com/specimen/${details.path}');
+                        }
+                      }
                     }),
               ),
               Expanded(
